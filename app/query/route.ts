f57357a6b2f4d75b51f262e6987460cc474880f7
sizaro <spartansiza@gmail.com>
2024-12-03 16:00:22 -0800

    app init

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,26 +1,24 @@
-// import { db } from "@vercel/postgres";
-
-// const client = await db.connect();
-
-// async function listInvoices() {
-// 	const data = await client.sql`
-//     SELECT invoices.amount, customers.name
-//     FROM invoices
-//     JOIN customers ON invoices.customer_id = customers.id
-//     WHERE invoices.amount = 666;
-//   `;
-
-// 	return data.rows;
-// }
+import { db } from '@vercel/postgres';
 
 export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
-  // try {
-  // 	return Response.json(await listInvoices());
-  // } catch (error) {
-  // 	return Response.json({ error }, { status: 500 });
-  // }
+  const client = await db.connect();
+
+  try {
+    await client.sql`BEGIN`;
+
+    const result = await client.sql`
+      SELECT invoices.amount, customers.name
+      FROM invoices
+      JOIN customers ON invoices.customer_id = customers.id
+      WHERE invoices.amount = 666;
+    `;
+
+    await client.sql`COMMIT`;
+
+    return Response.json(result.rows);
+  } catch (error) {
+    await client.sql`ROLLBACK`;
+
+    return Response.json({ error }, { status: 500 });
+  }
 }
